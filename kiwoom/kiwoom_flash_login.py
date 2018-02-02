c1720200ef15e8c7a3a88ac520374b1e0627cea0
angelxtry <angelxtry@gmail.com>
2018-02-02 21:11:25 +0900

    kiwoom update

---
+++ kiwoom/kiwoom_flash_login.py
@@ -0,0 +1,27 @@
+import time
+import os
+
+from pywinauto import application
+from pywinauto import timings
+
+from kiwoom.kiwoom_user_info import UserInfo
+
+app = application.Application()
+app.start("C:/Kiwoom/KiwoomFlash3/bin/NKMiniStarter.exe")
+
+title = "번개3 Login"
+dlg = timings.WaitUntilPasses(20, 0.5, lambda: app.window_(title=title))
+
+pass_ctrl = dlg.Edit2
+pass_ctrl.SetFocus()
+pass_ctrl.TypeKeys(UserInfo.pw)
+
+cert_ctrl = dlg.Edit3
+cert_ctrl.SetFocus()
+cert_ctrl.TypeKeys(UserInfo.cert)
+
+btn_ctrl = dlg.Button0
+btn_ctrl.Click()
+
+time.sleep(50)
+os.system("taskkill /im nkmini.exe")
--- kiwoom/simple_info_query.py
+++ kiwoom/simple_info_query.py
@@ -10,7 +10,10 @@ class MyWindow(QMainWindow):
         # Kiwoom Login
         self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
         self.kiwoom.dynamicCall("CommConnect()")
+
+        # OpenAPI+ Event
         self.kiwoom.OnEventConnect.connect(self.event_connect)
+        self.kiwoom.OnReceiveTrData.connect(self.receive_trdata)
 
         self.setWindowTitle("PyStock")
         self.setGeometry(300, 300, 300, 150)
@@ -38,9 +41,24 @@ class MyWindow(QMainWindow):
         code = self.code_edit.text()
         self.text_edit.append("종목코드: " + code)
 
+        # SetInputValue
+        self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
+
+        # CommRqData
+        self.kiwoom.dynamicCall("CommRqData(QString, QString, int, QString)", "opt10001_req", "opt10001", 0, "0101")
+
+    def receive_trdata(self, screen_no, rqname, trcode, recordname, prev_next, data_len, err_code, msg1, msg2):
+        if rqname == "opt10001_req":
+            name = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, 0, "종목명")
+            volume = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, 0, "거래량")
+
+            self.text_edit.append("종목명: " + name.strip())
+            self.text_edit.append("거래량: " + volume.strip())
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = MyWindow()
     myWindow.show()
     app.exec_()
+
