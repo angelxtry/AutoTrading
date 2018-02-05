4a9573ef4219cb7d6ad45b21a50976a536a36804
angelxtry <angelxtry@gmail.com>
2018-02-05 09:24:59 +0900

    PyQt code add

--- PyQt/using_ui.py
+++ PyQt/using_ui.py
@@ -9,6 +9,10 @@ class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.btn_clicked)
+
+    def btn_clicked(self):
+        QMessageBox.about(self, "message", "clicked")
 
 
 if __name__ == "__main__":
